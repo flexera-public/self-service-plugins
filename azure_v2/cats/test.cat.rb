ea6123af5c70d56512f3bf121dfdc69daf44e84f
Slava Khrenov <slava@rightscale.com>
2015-06-09 10:02:46 +0300

    added protocol to the host and new variables in the cat file

--- azure_v2/cats/test.cat.rb
+++ azure_v2/cats/test.cat.rb
@@ -4,16 +4,16 @@ short_description "Allows you to launch a machine on Azure"
 
 namespace "azure" do
   service do
-    host "cmdev-selfservice-403.test.rightscale.com"        # HTTP endpoint presenting an API defined by self-serviceto act on resources
-    path "/azure_plugin"      # path prefix for all resources, RightScale account_id substituted in for multi-tenancy
+    host "https://cmdev-selfservice-403.test.rightscale.com"  # HTTP endpoint presenting an API defined by self-service to act on resources
+    path "/azure_plugin"                       # path prefix for all resources, RightScale account_id substituted in for multi-tenancy
     headers do {
-      "user-agent" => "self_service"      # special headers as needed
+      "user-agent" => "self_service"           # special headers as needed
     } end
   end
   type "instance" do                           # defines resource of type "pod"
     provision "provision_instance"             # name of RCL definition to use to provision the resource
     delete "delete_instance"                   # name of RCL definition to use to delete the resource
-    path "/azure_plugin/instances"
+    path "/instances"
     fields do
       field "name" do
         type "string"
@@ -49,11 +49,25 @@ parameter "instance_size" do
   allowed_values "Standard_G1", "Standard_G2", "Standard_G3"
 end
 
+parameter "location" do
+  type "string"
+  label "Location"
+  description "Azure region"
+  default "West US"
+  allowed_values "West US", "East US", "Central US"
+end
+
+parameter "group" do
+  type "string"
+  label "Resource Group"
+  description "Resource group where instance will be launched"
+end
+
 resource "base_server", type: "azure.instance" do
   name                  $instance_name
   instance_type_uid     $instance_size
-  location              "westus"
-  group_name            "Group-1"
+  location              $location
+  group_name            $group
 end
 
 # Define the RCL definitions to create and destroy the resource
