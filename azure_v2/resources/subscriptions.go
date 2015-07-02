768e1d1fceb38541a2d234ac0f5fb45aba3b5fb1
Slava Khrenov <slava@rightscale.com>
2015-07-03 00:41:54 +0300

    generify Subscription resource

--- azure_v2/resources/subscriptions.go
+++ azure_v2/resources/subscriptions.go
@@ -20,6 +20,7 @@ type Subscription struct {
 	State          string      `json:"state"`
 	SubscriptionID string      `json:"subscriptionId"`
 	Policies       interface{} `json:"subscriptionPolicies"`
+	Href           string      `json:"href,omitempty"`
 }
 
 // SetupSubscriptionRoutes declares routes for Subscription resource
@@ -28,17 +29,39 @@ func SetupSubscriptionRoutes(e *echo.Echo) {
 	e.Get("/subscription", getSubscription)
 }
 
-// getSubscription return info about subscription provided in creds
 func getSubscription(c *echo.Context) error {
-	path := fmt.Sprintf("%s/%s/%s?api-version=%s", config.BaseURL, subscriptionsPath, *config.SubscriptionIDCred, "2015-01-01")
-	body, err := GetResource(c, path)
-	if err != nil {
-		return nil
-	}
-	var dat *Subscription
-	if err := json.Unmarshal(body, &dat); err != nil {
+	return Get(c, new(Subscription))
+}
+
+// GetPath returns full path to the sigle subscription
+func (s *Subscription) GetPath() string {
+	return fmt.Sprintf("%s/%s/%s?api-version=%s", config.BaseURL, subscriptionsPath, *config.SubscriptionIDCred, "2015-01-01")
+}
+
+// HandleResponse manage raw cloud response
+func (s *Subscription) HandleResponse(c *echo.Context, body []byte, actionName string) error {
+	if err := json.Unmarshal(body, &s); err != nil {
 		return eh.GenericException(fmt.Sprintf("got bad response from server: %s", string(body)))
 	}
-
-	return c.JSON(200, dat)
+	s.Href = s.GetHref("")
+	return nil
 }
+
+// GetContentType returns content type of subscription
+func (s *Subscription) GetContentType() string {
+	return "vnd.rightscale.subscription+json"
+}
+
+// GetHref returns subscription href
+func (s *Subscription) GetHref(_ string) string {
+	return "/subscription"
+}
+
+// GetResponseParams is accessor function for getting access to responseParams struct
+func (s *Subscription) GetResponseParams() interface{} { return nil }
+
+//GetCollectionPath is a fake function to support AzureResource by Subscription
+func (s *Subscription) GetCollectionPath(groupName string) string { return "" }
+
+//GetRequestParams is a fake function to support AzureResource by Subscription
+func (s *Subscription) GetRequestParams(c *echo.Context) (interface{}, error) { return "", nil }
