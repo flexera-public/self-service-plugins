add9e52e41df5e1bf7681461d547f7cc282c00df
Slava Khrenov <slava@rightscale.com>
2015-11-23 15:17:11 +0300

    CF-150 - fixed error handling by using genericError type as common error type for this app

--- azure_v2/error_handler/error_handler.go
+++ azure_v2/error_handler/error_handler.go
@@ -9,9 +9,13 @@ import (
 )
 
 type genericError struct {
-	Code    	int
-	Message 	string
-	StackTrace  string `json:"StackTrace,omitempty"`
+	Code       int
+	Message    string
+	StackTrace string `json:"StackTrace,omitempty"`
+}
+
+func (e *genericError) Error() string {
+	return e.Message
 }
 
 // AzureErrorHandler is a custom Echo.HTTPErrorHandler
@@ -20,14 +24,13 @@ func AzureErrorHandler(e *echo.Echo) echo.HTTPErrorHandler {
 		ge := new(genericError)
 		ge.Code = http.StatusInternalServerError // default status code is 500
 		ge.Message = http.StatusText(ge.Code)    // default message is 'Internal Server Error'
-		// if e.Debug() {
-		// 	ge.Message = err.Error() //show original error message in case of debug mode https://github.com/labstack/echo/blob/1e117621e9006481bfc0fd8e6bafab48c1848639/echo.go#L161
-		// }
+		if e.Debug() {
+			ge.Message = err.Error() //show original error message in case of debug mode https://github.com/labstack/echo/blob/1e117621e9006481bfc0fd8e6bafab48c1848639/echo.go#L161
+		}
 		switch errorType := err.(type) {
 		case *errors.Error:
-			if he, ok := errorType.Err.(*echo.HTTPError); ok {
-				ge.Code = he.Code()
-				ge.Message = he.Error()
+			if he, ok := errorType.Err.(*genericError); ok {
+				ge = he
 			}
 			if e.Debug() && ge.Code == 500 {
 				ge.StackTrace = errorType.ErrorStack()
@@ -43,16 +46,26 @@ func AzureErrorHandler(e *echo.Echo) echo.HTTPErrorHandler {
 
 // GenericException represents error with status code 400
 func GenericException(message string) error {
-	return errors.New(*echo.NewHTTPError(400, message))
+	return errors.New(&genericError{
+		Code:    400,
+		Message: message,
+	})
 }
+
 // RecordNotFound represents error with status code 404
 func RecordNotFound(resourceID string) error {
 	message := fmt.Sprintf("Could not find resource with id: %s", resourceID)
-	return errors.New(*echo.NewHTTPError(404, message))
+	return errors.New(&genericError{
+		Code:    404,
+		Message: message,
+	})
 }
 
 // InvalidParamException returns generic error massage for invalid value of paramName
 func InvalidParamException(paramName string) error {
 	message := fmt.Sprintf("You have specified an invalid '%s' parameter.", paramName)
-	return errors.New(*echo.NewHTTPError(400, message))
+	return errors.New(&genericError{
+		Code:    400,
+		Message: message,
+	})
 }
